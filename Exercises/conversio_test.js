8ff253e7c6d12295f98f0a97eb12a47fbfe3527c
CodeXand3rr <jballesteross235@gmail.com>
2024-04-24 15:36:32 -0400

    conversion test 1

---
+++ Exercises/conversio_test.js
@@ -0,0 +1,12 @@
+// description: this script tests various numeric conversion techniques 
+// Auntor Jahir Ballesteros 
+
+var a = " 101.1 ";
+var b = "55";
+var c = "402 Stevens";
+var d = "Number 5 ";
+
+aResult = parseInt (a)
+console.log(aResult);
+
+console.log(parseInt(a))
