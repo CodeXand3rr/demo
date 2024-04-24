7c89a801001c3e0dadc3bc0ec668be389b176e38
CodeXand3rr <jballesteross235@gmail.com>
2024-04-24 10:29:15 -0400

    area of a rectangle

---
+++ Exercises/MathScripts/are_of_rectangle.js
@@ -0,0 +1,8 @@
+var lenght = (4);
+var wide = (3)
+
+console.log(typeof lenght, typeof wide);
+
+console.log("the area of a rectangle is: " + (lenght * wide));
+
+console.log("the area of a rectangle is: 12")
