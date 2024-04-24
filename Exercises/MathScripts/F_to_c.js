ee7c3930f06f327df5c37eec77ffc832bff40556
CodeXand3rr <jballesteross235@gmail.com>
2024-04-24 15:29:02 -0400

    update

---
+++ Exercises/MathScripts/F_to_c.js
@@ -0,0 +1,9 @@
+"use strict";
+// define your iputs
+var fahrenhightTemperautre = 98;
+
+var celsiusTemperature;
+// do the calculation  C = 5/9 x (F-32)
+celsiusTemperature = ((fahrenhightTemperautre - 32) * 5) / 9;
+// get the results
+console.log(" fahrenhight temperature of " + fahrenhightTemperautre + "is" + celsiusTemperature + "celsius");
---
+++ Exercises/MathScripts/taxes.js
@@ -0,0 +1,8 @@
+// define your iputs
+var monthlySalary = 50000;
+var taxRate = 23 / 100;
+// do the calculation and save the result to a variable
+var taxAmount = monthlySalary * taxRate;
+
+// print the results
+console.log("You pay " + taxAmount.toFixed(2) + " in taxes");
