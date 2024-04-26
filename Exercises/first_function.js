ac9066d46a0f2d0c2e328ec70481092003de6cd2
CodeXand3rr <jballesteross235@gmail.com>
2024-04-26 13:15:46 -0400

    example

---
+++ Exercises/first_function.js
@@ -0,0 +1,14 @@
+"use strict"
+
+function greet() {
+  let message = "Hello World";
+  consolo.console.log(message);
+}
+
+function brushTeeth(){
+    console.log("get up");
+    console.log("brush teeth");
+}
+
+
+greet();
