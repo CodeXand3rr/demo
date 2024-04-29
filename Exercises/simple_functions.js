18eaeb373f3e02971f4d73aff01b9c2779cff1cc
CodeXand3rr <jballesteross235@gmail.com>
2024-04-29 15:39:17 -0400

    new

---
+++ Exercises/simple_functions.js
@@ -0,0 +1,17 @@
+function favoriteThings() {
+  console.log("Food");
+  console.log("water");
+  console.log("family");
+}
+
+function whyImHere() {
+  console.log("I wanted to learn something new");
+}
+
+function favoritePlace() {
+  console.log("My grandmothers house");
+}
+
+whyImHere();
+favoriteThings()
+favoritePlace()
