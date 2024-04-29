e49fe1a87a8c0be9afc292af7ace9fd9699db0d9
CodeXand3rr <jballesteross235@gmail.com>
2024-04-29 14:27:12 -0400

    calculator

--- 1/index.js
+++ 1/index.js
@@ -1,9 +1,26 @@
 "use strict";
 
 function add() {
-  var number1 = 200;
-  var number2 = 200;
-  var result = number1 + number2;
-  console.log(result);
+
+var numberOneInput = document.getElementById("numberOneInput")
+var numberTwoInput = document.getElementById("numberTwoInput")
+
+//   var numberOne = numberOneInput.value;
+var numberOne = Number (numberOneInput.value);
+
+//   var numberTwo = numberTwoInput.value;
+ var numberTwo = Number(numberTwoInput.value);
+ 
+  var result = numberOne + numberTwo;
+  var resultInput = document.getElementById("resultInput");
+  resultInput.value=result
 }
-add();
+// add();
+function init() {
+  var addButton = document.getElementById("addButton");
+  console.log(addButton);
+  addButton.onclick = add;
+}
+
+// init();
+window.onload = init;
